d53a475081825b7043384aa07185107e13babdc3
Nathan <nhamilton@fundsforlearning.com>
2023-11-27 15:22:16 -0600

    feat: sending inital get route

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const morgan = require("morgan")
+const {list, find} = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
+app.use(express.static('./public'))
+
+app.get("/", (req, res) => {
+  const posts = list(); 
+
+  const html = 
+`  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `
+      <li>
+        <h4>${post.title}</h4>
+        <h5>${post.name}</h5>
+      </li>`).join("")}
+    </ul>
+  </body>
+  </html>`
+  res.send(html)
+});
 
 const PORT = 1337;
 
